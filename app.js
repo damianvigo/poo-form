ed20551d2db8aeb3fbb180bb2f210d04d81aed89
Damian Vigo <damiandanielvigo@gmail.com>
2022-04-29 08:50:42 -0300

    add-validations

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const $formulario = document.querySelector('#formulario'),
-  $cardEstudiantes = document.querySelector('#cardsEstudiantes'),
-  $cardsProfesores = document.querySelector('#cardsProfesores'),
-  $templateEstudiante = document.querySelector('#templateEstudiante').content,
-  $templateProfesor = document.querySelector('#templateProfesor').content;
+import { uid } from './node_modules/uid/dist/index.mjs';
+
+const d = document,
+  $formulario = d.querySelector('#formulario'),
+  $cardEstudiantes = d.querySelector('#cardsEstudiantes'),
+  $cardsProfesores = d.querySelector('#cardsProfesores'),
+  $templateEstudiante = d.querySelector('#templateEstudiante').content,
+  $templateProfesor = d.querySelector('#templateProfesor').content,
+  $alert = d.querySelector('.alert'),
+  $alertName = document.getElementById('alertName'),
+  $alertEmail = document.getElementById('alertEmail'),
+  $userName = document.getElementById('userName'),
+  $userEmail = document.getElementById('userEmail');
 
 const estudiantes = [],
   profesores = [];
@@ -34,10 +42,65 @@ document.addEventListener('click', (e) => {
 $formulario.addEventListener('submit', (e) => {
   e.preventDefault();
 
+  const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  const regUserEmail =
+    /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+
+  const errores = [];
+  console.log(errores);
+
+  const pintarMensajeError = (errores) => {
+    errores.forEach((item) => {
+      item.tipo.classList.remove('d-none');
+      item.tipo.textContent = item.msg;
+    });
+  };
+
   const datos = new FormData($formulario);
   console.log(datos);
   const [nombre, email, opcion] = [...datos.values()];
   console.log(nombre, email, opcion);
+
+  if (!nombre.trim() || !email.trim() || !opcion.trim()) {
+    $alert.classList.remove('d-none');
+  } else {
+    $alert.classList.add('d-none');
+    $userName.classList.remove('is-invalid');
+  }
+
+  // validar nombre
+  if (!regUserName.test(nombre)) {
+    $userName.classList.add('is-invalid');
+    /* $alertName.classList.remove('d-none'); */
+
+    errores.push({
+      tipo: $alertName,
+      msg: 'Formato no válido campo nombre, solo letras',
+    });
+  } else {
+    $userName.classList.remove('is-invalid');
+    $userName.classList.add('is-valid');
+    $alertName.classList.add('d-none');
+  }
+
+  // validar email
+  if (!regUserEmail.test($userEmail.value)) {
+    $userEmail.classList.add('is-invalid');
+
+    errores.push({
+      tipo: $alertEmail,
+      msg: 'Escriba un correo válido',
+    });
+  } else {
+    $userEmail.classList.remove('is-invalid');
+    $userEmail.classList.add('is-valid');
+    $alertEmail.classList.add('d-none');
+  }
+
+  if (errores.length !== 0) {
+    pintarMensajeError(errores);
+    return;
+  }
 
   if (opcion === 'Estudiante') {
     const estudiante = new Estudiante(nombre, email);
@@ -60,7 +123,8 @@ class Persona {
   constructor(nombre, email) {
     this.nombre = nombre;
     this.email = email;
-    this.uid = `${Date.now()}`;
+    this.uid = uid();
+    /* this.uid = `${Date.now()}`; */
   }
 
   static pintarPersonaUI(personas, tipo) {
